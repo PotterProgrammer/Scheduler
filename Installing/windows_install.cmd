fcab75eade05c59120b4a6de545ebc5657197d68
Garry Taylor <gft@gftaylor.com>
2025-05-06 21:49:22 -0500

    Starting on Windows install

---
+++ Installing/windows_install.cmd
@@ -0,0 +1,50 @@
+@echo off
+
+set PORT=3000
+
+set _showhelp=0
+set _skipPerl=0
+
+if "%1" == "/?" ( set _showhelp=1)
+if "%1" == "/h" ( set _showhelp=1)
+if "%1" == "-h" ( set _showhelp=1)
+if "%1" == "--h" ( set _showhelp=1)
+
+if "%1" == "/skipPerl" ( set _skipPerl=1 & shift)
+
+if %_showhelp% equ 1 (
+   echo ^windows_install:  A batch file to install Scheduler on Windows
+   echo ^  usage:  windows_install [/?] [port_number]
+   echo ^     where:
+   echo ^     port_number    is the number of the port that Scheduler should
+   echo ^                    listen to for incoming connections.  If this is
+   echo ^                    not specified, it defaults to 3000.
+   exit /b 0
+)
+
+:: Get the port number, if the user specified one
+if NOT "%1" == "" ( set PORT=%1)
+
+
+:: Now, start by downloading Strawberry Perl
+curl -L https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_54001_64bit_UCRT/strawberry-perl-5.40.0.1-64bit-portable.zip -o strawberryPerl.zip
+
+:: Unpack perl
+mkdir  StrawberryPerl
+cd StrawberryPerl
+tar -xf ..\strawberryPerl.zip 
+
+:: Use cpanm in Perl to install remaining pieces
+cd ..
+PATH=%CD%\StrawberryPerl\site\bin;%CD%\StrawberryPerl\perl\bin;%CD%\StrawberryPerl\c\bin;%PATH%
+
+:: Install troublesome pieces first
+cpanm --force --cpanfile windows_forced_cpanfile --installdeps .
+
+:: Then install the rest
+cpanm --cpanfile windows_cpanfile --installdeps .
+
+
+:: Build program to start Scheduler
+echo %PATH% >launchScheduler.cmd
+echo perl Scheduler daemon -l http://*:%PORT% >> launchScheduler.cmd
